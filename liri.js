daa103ab17c4eb4953175b975362bf350ddc42f7
dnl092 <drogalsky14@my.whitworth.edu>
2019-06-25 20:39:11 -0700

    added spotify app command

--- liri.js
+++ liri.js
@@ -1,5 +1,37 @@
 require("dotenv").config();
 
-let keys = require('./key.js');
+let keys = require('./key');
+let Spotify = require('node-spotify-api');
 
-var spotify = new spotify(keys.spotify);
+let spotify = new Spotify(keys.spotify);
+
+let command = process.argv[2];
+
+let theQuery = process.argv.slice(3);
+var liri = {
+    "spotify-this-song": function (theSong) {
+        if (theSong.length === 0) {
+            theSong = "The Sign"
+            console.log(theSong);
+        }
+        console.log('Let me get that information for you.')
+        spotify
+            .search({type: 'track', query: theSong})
+            .then(function (data) {
+                console.log(
+                    `Artist: ${data.tracks.items[0].artists[0].name}\n`,
+                    `Album: ${data.tracks.items[0].album.name}\n`,
+                    `Song: ${data.tracks.items[0].name} \n`,
+                    `Preview: ${data.tracks.items[0].preview_url} \n`
+                );
+            })
+            .catch(function (err) {
+                console.log('Error occurred: ' + err);
+            })
+    },
+    'concert-this': function() {
+        
+    }
+}
+
+liri[command](theQuery)
